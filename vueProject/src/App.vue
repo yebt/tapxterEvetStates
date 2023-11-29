3a9b6294fcceb29a873ad120e1de4b82a3921ce3
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-11-29 18:39:34 -0500

    Feat: build

--- vueProject/src/App.vue
+++ vueProject/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref, computed } from 'vue'
+import { onMounted, ref } from 'vue'
 // import HelloWorld from './components/HelloWorld.vue'
 
 // fetch data from http://tapxter.test/report/users-evet/2
@@ -14,6 +14,7 @@ const infoData = {}
 const arrayReport = ref([])
 const totalPersons = ref(0)
 const totalVerified = ref(0)
+const firstLoad = ref(false)
 //
 
 const getData = async () => {
@@ -25,6 +26,18 @@ const getData = async () => {
   })
   const data = await response.json()
   return data
+}
+
+const saveDataInLocalStorage = () => {
+  localStorage.setItem('lastUpdate', lastUpdate.value)
+  localStorage.setItem('userData', JSON.stringify(userData.value))
+  localStorage.setItem('infoData', JSON.stringify(infoData.value))
+}
+
+const loadDataFromLocalStorage = () => {
+  lastUpdate.value = localStorage.getItem('lastUpdate')
+  userData.value = JSON.parse(localStorage.getItem('userData'))
+  infoData.value = JSON.parse(localStorage.getItem('infoData'))
 }
 
 const trateData = async () => {
@@ -90,8 +103,10 @@ function procesarDatos (data) {
   return resultado
 }
 
-onMounted(() => {
-  trateData()
+onMounted(async () => {
+  firstLoad.value = true
+  await trateData()
+  firstLoad.value = false
   setInterval(trateData, 30000)
   // console.log(procesarDatos(trstData))
 })
@@ -99,38 +114,52 @@ onMounted(() => {
 
 <template>
   <main class="main-container overflow-hidden">
-  <div class="edges">
-    <p class="baddger">
-      Última actualización:&nbsp;<code class="font-mono font-bold">{{lastUpdate}}</code>
-    </p>
-    <div class="logo-container items-center lg:relative lg:p-2">
-      <div
-        class="max-lg:hidden absolute  inset-0 bg-gradient-to-l from-martinique-500/20  from-1%  to-transparent to-90%  w-[200%]">
+    <div class="edges">
+      <p class="baddger">
+        Última actualización:&nbsp;<code class="font-mono font-bold">{{ lastUpdate }}</code>
+      </p>
+      <div class="logo-container items-center lg:relative lg:p-2">
+        <div
+          class="max-lg:hidden absolute  inset-0 bg-gradient-to-l from-martinique-500/20  from-1%  to-transparent to-90%  w-[200%]">
+        </div>
+        <img alt="Uni Logo" fetchpriority="high" width="180" height="24" decoding="async" data-nimg="1" class="mx-4 "
+          style="" src="./assets/images/logo-uni.svg" />
+        <a class="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+          href="https://app.tapxter.com" target="_blank" rel="noopener noreferrer">
+          By
+          <img alt="Tapxter Logo" fetchpriority="high" width="90" height="24" decoding="async" data-nimg="1" class=""
+            style="" src="./assets/images/logo-tapxter.svg" />
+        </a>
       </div>
-      <img alt="Uni Logo" fetchpriority="high" width="180" height="24" decoding="async" data-nimg="1" class="mx-4 "
-        style="" src="./assets/images/logo-uni.svg" />
-      <a class="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-        href="https://app.tapxter.com" target="_blank" rel="noopener noreferrer">
-        By
-        <img alt="Tapxter Logo" fetchpriority="high" width="90" height="24" decoding="async" data-nimg="1" class=""
-          style="" src="./assets/images/logo-tapxter.svg" />
-      </a>
     </div>
-  </div>
-  <div class="backgrower before:bg-gradient-radial flex-1 w-full justify-center">
-    <section class="flex max-lg:flex-col justify-around items-center w-full max-lg:gap-8 md:mx-10">
-      <!-- Counters -->
-      <article class="">
-        <a href="#"
-          class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-martinique-100 dark:bg-gray-800/50 dark:border-gray-700 dark:hover:bg-gray-700">
+    <div class="backgrower before:bg-gradient-radial flex-1 w-full justify-center">
+      <section class="flex max-lg:flex-col justify-around items-center w-full max-lg:gap-8 md:mx-10">
+        <!-- Counters -->
+        <article class="">
+          <a href="#"
+            class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-martinique-100 dark:bg-gray-800/50 dark:border-gray-700 dark:hover:bg-gray-700">
 
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-violet-300 uppercase">
-            asistencia al evento
-          </h5>
-          <p class="font-normal text-gray-700 dark:text-gray-400 flex text-xl">
-            Persosnas validadas
-          </p>
-            <h2 class="mb-2 text-8xl font-bold tracking-tight text-gray-900 dark:text-white">{{ totalVerified }}</h2>
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-violet-300 uppercase">
+              asistencia al evento
+            </h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400 flex text-xl">
+              Persosnas validadas
+            </p>
+            <div role="status">
+              <svg aria-hidden="true" v-if="firstLoad"
+                class="inline w-20 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300"
+                viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor" />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill" />
+              </svg>
+              <span class="sr-only">Loading...</span>
+            </div>
+            <h2 v-if="!firstLoad" class="mb-2 text-8xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {{ totalVerified }}<span>-></span></h2>
             <p class="font-normal text-gray-700 dark:text-gray-400 flex text-xl">
               <span class="flex-1"> Persosnas invitadas </span> <code class="font-mono">{{ totalPersons }}</code>
             </p>
@@ -153,96 +182,35 @@ onMounted(() => {
 
     <!-- <div class="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"> -->
     <div class="mb-32 flex text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left mt-3">
-
       <div
-        class="flex gap-4 max-lg:flex-col w-max lg:animate-marquee [--duration:30s] lg:hover:[animation-play-state:paused] max-lg:mt-3">
+        class="flex gap-4 max-lg:flex-col lg:w-max lg:animate-marquee [--duration:50s] lg:hover:[animation-play-state:paused] max-lg:mt-3">
         <template v-for="(item, index) in arrayReport" :key="index">
           <template v-if="item !== 'undefined'">
-            <a href="#" @click.prevent=""
-            class="simple-card-border min-w-[800px]" target="_blank" rel="noopener noreferrer">
-            <h2 class="mb-3 text-2xl font-semibold flex">
-              <span class="flex-1">{{ item }}</span>
-              <span class="inline-block transition-transform  ">
-                <code class="font-mono">{{ infoData.value.groupeds[item].total }}</code>
-              </span>
-            </h2>
-            <p class="m-0 -max-w-[30ch] text-sm opacity-50">
-              <template v-for="(subitem, index) in Object.keys(infoData.value.groupeds[item].deportes)" :key="index">
-                <template v-if="subitem != 'undefined'">
-                  <span>{{ subitem }}: <code class="font-mono">{{ infoData.value.groupeds[item].deportes[subitem] }}</code></span><span class="px-1">/</span>
+            <a href="#" @click.prevent="" class="simple-card-border w-auto max-w-[600px] lg:min-w-[800px]" target="_blank"
+              rel="noopener noreferrer">
+              <h2 class="mb-3 text-2xl font-semibold flex">
+                <span class="flex-1">{{ item }}</span>
+                <span class="inline-block transition-transform  ">
+                  <code class="font-mono">{{ infoData.value.groupeds[item].total }}</code>
+                </span>
+              </h2>
+              <p class="m-0 -max-w-[30ch] text-sm opacity-50">
+                <template v-for="(subitem, index) in Object.keys(infoData.value.groupeds[item].deportes)" :key="index">
+                  <template v-if="subitem != 'undefined'">
+                    <span>{{ subitem }}: <code
+                        class="font-mono">{{ infoData.value.groupeds[item].deportes[subitem] }}</code></span><span
+                      class="px-1">/</span>
+                  </template>
                 </template>
-              </template>
-            </p>
-          </a>
+              </p>
+            </a>
           </template>
 
         </template>
 
       </div>
-
-      <!-- <a href="https://nextjs.org/docs?utm_source=create-next-app&amp;utm_medium=appdir-template&amp;utm_campaign=create-next-app"
-                      class="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                      target="_blank" rel="noopener noreferrer">
-                      <h2 class="mb-3 text-2xl font-semibold">
-                        Docs
-                        <span
-                          class="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
-                      </h2>
-                                      <p class="m-0 max-w-[30ch] text-sm opacity-50">
-                          Find in-depth information about Next.js features and API.
-                        </p>
-                      </a> -->
-      <!-- <a href="https://nextjs.org/learn?utm_source=create-next-app&amp;utm_medium=appdir-template-tw&amp;utm_campaign=create-next-app"
-                      class="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                      target="_blank" rel="noopener noreferrer">
-                      <h2 class="mb-3 text-2xl font-semibold">
-                        Learn
-                        <span
-                          class="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
-                      </h2>
-                      <p class="m-0 max-w-[30ch] text-sm opacity-50">
-                        Learn about Next.js in an interactive course with&nbsp;quizzes!
-                      </p>
-                    </a><a
-                      href="https://vercel.com/templates?framework=next.js&amp;utm_source=create-next-app&amp;utm_medium=appdir-template&amp;utm_campaign=create-next-app"
-                      class="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                      target="_blank" rel="noopener noreferrer">
-                      <h2 class="mb-3 text-2xl font-semibold">
-                        Templates
-                        <span
-                          class="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
-                      </h2>
-                      <p class="m-0 max-w-[30ch] text-sm opacity-50">
-                        Explore the Next.js 13 playground.
-                      </p>
-                    </a><a
-                      href="https://vercel.com/new?utm_source=create-next-app&amp;utm_medium=appdir-template&amp;utm_campaign=create-next-app"
-                      class="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                      target="_blank" rel="noopener noreferrer">
-                      <h2 class="mb-3 text-2xl font-semibold">
-                        Deploy
-                        <span
-                          class="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
-                      </h2>
-                                    <p class="m-0 max-w-[30ch] text-sm opacity-50">
-                                      Instantly| deploy your Next.js site to a shareable URL with Vercel.
-                                    </p>
-                                  </a> -->
     </div>
   </main>
 </template>
 
-<style scoped>
-/* .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-} */
-</style>
+<style scoped></style>
