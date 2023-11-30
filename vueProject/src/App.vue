7628df906dcd40cccdaf96c96d5e23a3b562e095
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2023-11-30 09:04:24 -0500

    Feat: pass the url

--- vueProject/src/App.vue
+++ vueProject/src/App.vue
@@ -4,8 +4,8 @@ import { onMounted, ref } from 'vue'
 
 // fetch data from http://tapxter.test/report/users-evet/2
 // const URL = 'http://tapxter.test/report/users-evet/2'
-// const URL = 'https://tapxter-api-df689ec6366f4275d888.tapxter.com/report/users-evet/2'
-// const BEARER = 'q1l439iHRlSv3HaokMblMFhEVUbSN8zcQZrNX36cRe4=.eyJpZGVoYXNoX3VzciI6MywidmlydGFnX2hhc2giOiI3MThmNWM4Zjk4ZjgzIiwicGVyZmlsIjoidXNlciIsImVtYWlsIjoieWlsbWFyLmkuYW1AZ21haWwuY29tIiwibG9naW5fdXNyIjoieWlsbWFyLmkuYW0iLCJkYXRhX3VzciI6IntcIm5vbWJyZVwiOlwiWWlsbWFyIEFsemF0ZSBSb2RyaWd1ZXpcIiwgXCJhcGVsbGlkb3NcIjpcIlwiLCBcInRlbGVmb25vXCI6XCIzMTM3MDYwODgyXCJ9In0=.q1l439iHRlSv3HaokMblMPZs9UhKICK1+UT+sn00YB2iTH74IvEbpjfUVLAAoF0am+202msuoF5tPS43AnlmrIcxrzwxE+Do6qMNAWkIM1lVDES6pVzPd4b4eJiESBHa/RLCGg6j2jvwv1zNdh6fuxTmuHUniosn4S6POgrltcceh5+mG5tnliX7hOlutrXI0pUPCUk1/ONdY/Ym+8+NzZzBHthdztDOM+++eBD4rdHKC4ZdCvid7pcmslb/xKXlWOvNFsJQ0TZ1n7WIc0X0vBS83BvzAFTkZwBvxfNOLpDMWL7Gh87rKTDleYp/zINtaiqqYxdXNRLiVlce8mxfQg=='
+const URL = 'https://tapxter-api-df689ec6366f4275d888.tapxter.com/report/users-evet/2'
+const BEARER = 'q1l439iHRlSv3HaokMblMFhEVUbSN8zcQZrNX36cRe4=.eyJpZGVoYXNoX3VzciI6MywidmlydGFnX2hhc2giOiI3MThmNWM4Zjk4ZjgzIiwicGVyZmlsIjoidXNlciIsImVtYWlsIjoieWlsbWFyLmkuYW1AZ21haWwuY29tIiwibG9naW5fdXNyIjoieWlsbWFyLmkuYW0iLCJkYXRhX3VzciI6IntcIm5vbWJyZVwiOlwiWWlsbWFyIEFsemF0ZSBSb2RyaWd1ZXpcIiwgXCJhcGVsbGlkb3NcIjpcIlwiLCBcInRlbGVmb25vXCI6XCIzMTM3MDYwODgyXCJ9In0=.q1l439iHRlSv3HaokMblMPZs9UhKICK1+UT+sn00YB2iTH74IvEbpjfUVLAAoF0am+202msuoF5tPS43AnlmrIcxrzwxE+Do6qMNAWkIM1lVDES6pVzPd4b4eJiESBHa/RLCGg6j2jvwv1zNdh6fuxTmuHUniosn4S6POgrltcceh5+mG5tnliX7hOlutrXI0pUPCUk1/ONdY/Ym+8+NzZzBHthdztDOM+++eBD4rdHKC4ZdCvid7pcmslb/xKXlWOvNFsJQ0TZ1n7WIc0X0vBS83BvzAFTkZwBvxfNOLpDMWL7Gh87rKTDleYp/zINtaiqqYxdXNRLiVlce8mxfQg=='
 
 const lastUpdate = ref('')
 const userData = []
@@ -21,7 +21,7 @@ const getData = async () => {
   const response = await fetch(URL, {
     method: 'GET',
     headers: {
-      // Authorization: 'Bearer ' + BEARER
+      Authorization: 'Bearer ' + BEARER
     }
   })
   const data = await response.json()
@@ -143,7 +143,7 @@ onMounted(async () => {
               asistencia al evento
             </h5>
             <p class="font-normal text-gray-700 dark:text-gray-400 flex text-xl">
-              Persosnas validadas
+              Personas validadas
             </p>
             <div role="status" class="flex items-center justify-center m-2">
               <svg aria-hidden="true" v-if="firstLoad"
@@ -161,7 +161,7 @@ onMounted(async () => {
             <h2 v-if="!firstLoad" class="mb-2 text-8xl font-bold tracking-tight text-gray-900 dark:text-white">
               {{ totalVerified }}<span>-></span></h2>
             <p class="font-normal text-gray-700 dark:text-gray-400 flex text-xl">
-              <span class="flex-1"> Persosnas invitadas </span> <code class="font-mono">{{ totalPersons }}</code>
+              <span class="flex-1"> Personas invitadas </span> <code class="font-mono">{{ totalPersons }}</code>
             </p>
           </a>
         </article>
